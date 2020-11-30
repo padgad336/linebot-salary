3d410d809b09f5c4a74cd015f544a1c1fc2b0e5a
padgad <phairatwork@gmail.com>
2020-11-30 12:06:19 +0700

    add start

---
+++ functions/config.js
@@ -0,0 +1,16 @@
+const lineCredential = {
+    ACCESS_TOKEN: 'N9A5Hhx8ZaNdSKLGFFcTYbSUKfdLYH9PrtWEAklTJpA9FFsK3VqWysLUURCHPAilT2OUCFTQS6PAYFYrn0jcmzjfo6FlU9LR9G9vZnnbb/klU4cwuO7v1uzfTRWDUveJShl1xlCZRhD3gshpX92S0QdB04t89/1O/w1cDnyilFU='
+}
+const googleSheetCredential = {
+    SPREADSHEET_ID: '1keosLNCy8JOkA3kSBi4DDvxr9IZXWEVWc9QzYpVjoa8',
+    GOOGLE_SHEET: 'salary',
+    RANGE: 'A2:E'
+}
+const firebaseCredential = {
+    DATABASE_URL: 'https://salary-dev-padgad.firebaseio.com'
+}
+module.exports = {
+    lineCredential,
+    googleSheetCredential,
+    firebaseCredential
+}
---
+++ functions/index.js
@@ -0,0 +1,69 @@
+const functions = require('firebase-functions')
+
+const { googleSheetCredential } = require('./config')
+const { reply } = require('./helpers/line')
+const { salaryMessage } = require('./helpers/line/messages')
+const { getGoogleSheetData } = require('./helpers/googleSheets')
+const { validateRegistered, registerUser } = require('./helpers/firebase')
+
+exports.lineWebhook = functions.https.onRequest(async (req, res) => {
+    try {
+        const { type, message, source: { userId: lineUserID } } = req.body.events[0]
+        const isTextMessage = type === 'message' && message.type === 'text'
+
+        if (isTextMessage) {
+            const messageFromUser = message.text.trim()
+            const checkRegister = messageFromUser.split('register:')
+            const needToRegister = checkRegister && checkRegister[1]
+
+            if (needToRegister) {
+                const idCardForRegister = checkRegister[1]
+                const hasBeenRegistered = await validateRegistered(lineUserID)
+
+                if (hasBeenRegistered) {
+                    return replyMessage(req.body, res, 'ไม่สามารถลงทะเบียนซ้ำได้')
+                }
+
+                const employees = await getGoogleSheetData(googleSheetCredential.GOOGLE_SHEET, googleSheetCredential.RANGE)
+                const hasEmployee = employees.values.some(([employeeIDCard]) => employeeIDCard === idCardForRegister.toString())
+
+                if (!hasEmployee) {
+                    return replyMessage(req.body, res, 'เลขบัตรประชาชนไม่ตรงกับที่มีในระบบ')
+                }
+
+                registerUser(lineUserID, idCardForRegister)
+                return replyMessage(req.body, res, 'ลงทะเบียนเรียบร้อย')
+            } else {
+                switch (messageFromUser) {
+                    case 'register':
+                        return replyMessage(req.body, res, 'กรุณากรอก register:เลขบัตรบัตรประชาชน เช่น register:1234567890123')
+                    case 'salary':
+                        const hasBeenRegistered = await validateRegistered(lineUserID)
+
+                        if (!hasBeenRegistered) {
+                            return replyMessage(req.body, res, 'กรุณาลงทะเบียนก่อนใช้งาน')
+                        }
+
+                        const { idCard } = hasBeenRegistered
+                        const employees = await getGoogleSheetData(googleSheetCredential.GOOGLE_SHEET, googleSheetCredential.RANGE)
+                        const me = employees.values.filter(([employeeIDCard]) => employeeIDCard === idCard.toString())[0]
+
+                        return replyMessage(req.body, res, salaryMessage(me), 'flex')
+                    case 'setting':
+                        return replyMessage(req.body, res, 'ยังไม่เปิดใช้งานส่วนนี้')
+                }
+            }
+        }
+
+        res.status(200).send('ok')
+    } catch (error) {
+        console.error(error.message)
+        res.status(400).send('error')
+    }
+})
+
+const replyMessage = (bodyRequest, res, message, type) => {
+    reply(bodyRequest, message, type)
+
+    return res.status(200).send('ok')
+}
